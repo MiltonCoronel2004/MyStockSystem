79daa06e197b95f9c71c42707fb7c7d2d020d570
Milton <miltoncoronel2004@gmail.com>
2025-11-02 15:04:38 -0300

    added mysql2 dialect

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,13 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
+import mysql2 from "mysql2";
 dotenv.config();
 
 export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT || 3306,
-  dialect: "mysql",
+  dialect: "mysql2",
+  dialectModule: mysql2,
   dialectOptions: {
     ssl: {
       require: true,
