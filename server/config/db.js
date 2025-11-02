bdd4d0c05ad7ceb2ac50a93d538e84d3b0f37a57
Milton <miltoncoronel2004@gmail.com>
2025-11-02 14:45:36 -0300

    for aiven db changes

--- server/config/db.js
+++ server/config/db.js
@@ -1,21 +1,16 @@
-import { Sequelize } from "sequelize"
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
 
-export const sequelize = new Sequelize(
-  process.env.NAME_DB,
-  process.env.USER_DB,
-  process.env.PASS_DB,
-  {
-    host: process.env.HOST_DB,
-    port: process.env.PORT_DB,
-    dialect: process.env.DIALECT_DB
-    // dialectOptions: {
-    //
-    //   // Forzar para evitar SSL de bases de datos remota como de Render
-    //   ssl: {
-    //     require: true,
-    //     rejectUnauthorized: false
-    //   }
-    // },
-    //
-  })
-
+export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
+  dialect: "mysql",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+  logging: false,
+});
