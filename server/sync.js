8c61a87bb53832b751f5cacb5592f19cf3e85d8e
Milton <miltoncoronel2004@gmail.com>
2025-11-02 15:53:57 -0300

    added models to sync file

--- server/sync.js
+++ server/sync.js
@@ -1,15 +1,24 @@
-// Sync with Aiven Mysql DB
+// sync.js
+// === Sincronización con base de datos Aiven MySQL ===
+
 import { sequelize } from "./config/db.js";
+
+// Importar todos los modelos definidos
+import "./models/User.js";
+import "./models/Product.js";
+import "./models/TokenBlacklist.js";
+
 
 (async () => {
   try {
     await sequelize.authenticate();
     console.log("✅ Conexión establecida correctamente");
-    await sequelize.sync({ alter: true });
+    await sequelize.sync({ alter: true }); // crea o actualiza tablas según los modelos
     console.log("✅ Tablas sincronizadas correctamente");
   } catch (err) {
     console.error("❌ Error al sincronizar:", err);
   } finally {
+    await sequelize.close();
     process.exit();
   }
 })();
