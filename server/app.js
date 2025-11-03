044740d00553608bb3f267352f554e7787b9ee3c
Milton <miltoncoronel2004@gmail.com>
2025-11-02 23:15:38 -0300

    improvements

--- server/app.js
+++ server/app.js
@@ -20,13 +20,13 @@ app.use(userRoutes);
 app.use(productRoutes);
 
 // sincroniza DB una vez por invocación
-app.use(async (_req, res, next) => {
+app.use(async (_req, _res, next) => {
   try {
     await sequelize.authenticate();
     next();
   } catch (err) {
     console.error("Error conexión DB", err);
-    res.status(500).json({ error: "DB error" });
+    _res.status(500).json({ error: "DB error" });
   }
 });
 
