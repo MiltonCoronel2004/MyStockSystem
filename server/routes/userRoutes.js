f9b90be2948fd221a0ccbf8a1c95ae3e430438ba
Milton <miltoncoronel2004@gmail.com>
2025-11-02 23:16:26 -0300

    improvements

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getUsers, createUser, loginUser, logoutUser, getLoggedInfo } from "../controllers/userController.js";
+import { getUsers, createUser, loginUser, logoutUser } from "../controllers/userController.js";
 import { createUserValidator, loginUserValidator } from "../middlewares/userValidator.js";
 import { handleValidation } from "../middlewares/expressValidator.js";
 import { verifyAuth } from "../middlewares/authMiddleware.js";
@@ -11,4 +11,3 @@ userRoutes.post("/login", loginUserValidator, handleValidation, loginUser);
 
 userRoutes.get("/getusers", verifyAuth, getUsers);
 userRoutes.post("/logout", verifyAuth, logoutUser);
-// userRoutes.get("/logged", getLoggedInfo);
